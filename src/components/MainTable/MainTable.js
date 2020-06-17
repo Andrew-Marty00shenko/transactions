1102f7ab9b33f5f0f346d4e59cf1814204ec5ecf
Andrew <56186268+Andrew-Marty00shenko@users.noreply.github.com>
2020-06-17 15:55:58 +0300

    refactor

--- src/components/MainTable/MainTable.js
+++ src/components/MainTable/MainTable.js
@@ -1,14 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './MainTable.scss'
 import { Table } from 'reactstrap';
 import { Button, ButtonGroup } from 'reactstrap';
 import Filters from '../Filters/Filters'
 
 const MainTable = ({ list }) => {
-    const [filter, setFilter] = useState(list);
+    const [filter, setFilter] = useState([]);
+    const [activeIncome, setActiveIncome] = useState(false);
+    const [activeConsuption, setActiveConsuption] = useState(false);
+    const [activeValue, setActiveValue] = useState(false);
+    const [activeDate, setActiveDate] = useState(false);
     const [activeBtn, setActiveBtn] = useState([]);
 
-    const onCheckboxBtnClick = (selected) => {
+    useEffect(() => {
+        setFilter(list)
+        if (activeIncome) {
+            const currState = [...filter]
+            const newState = currState.filter(type => type.type === "доход");
+            const sortState = newState.sort((a, b) => b.value - a.value);
+            setFilter(sortState);
+        }
+        if (activeConsuption) {
+            const currState = [...filter];
+            const newState = currState.filter(type => type.type === "расход");
+            const sortState = newState.sort((a, b) => b.value - a.value);
+            setFilter(sortState);
+        }
+        if (activeDate) {
+            const date = new Date();
+            const currState = [...filter];
+            const newState = currState.filter(d =>
+                new Date(d.date) >=
+                new Date(String(date.getFullYear()) +
+                    "-" +
+                    String(date.getMonth() + 1) + "-01"
+                )
+            )
+            const sortState = newState.sort((a, b) => b.value - a.value);
+            setFilter(sortState);
+        }
+        if (activeValue) {
+            const currState = [...filter];
+            const newState = currState.filter(value => value.value > 1000);
+            const sortState = newState.sort((a, b) => b.value - a.value);
+            setFilter(sortState);
+
+        }
+    }, [activeIncome, activeConsuption, activeValue, activeDate])
+
+    const onCheckboxBtnClick = useCallback(selected => {
         const index = activeBtn.indexOf(selected);
         if (index < 0) {
             activeBtn.push(selected);
@@ -16,44 +56,25 @@ const MainTable = ({ list }) => {
             activeBtn.splice(index, 1);
         }
         setActiveBtn([...activeBtn]);
-    }
+    }, [activeBtn])
 
     const filterByIncome = () => {
-        const currState = [...filter]
-        const newState = currState.filter(type => type.type === "доход");
-        const sortState = newState.sort((a, b) => b.value - a.value)
-        setFilter(sortState);
+        setActiveIncome(!activeIncome)
         onCheckboxBtnClick(1);
     }
 
     const filterByConsuption = () => {
-        const currState = [...filter];
-        const newState = currState.filter(type => type.type === "расход");
-        const sortState = newState.sort((a, b) => b.value - a.value);
-        setFilter(sortState);
+        setActiveConsuption(!activeConsuption);
         onCheckboxBtnClick(2);
     }
 
     const filterByValue = () => {
-        const currState = [...filter];
-        const newState = currState.filter(value => value.value > 1000);
-        const sortState = newState.sort((a, b) => b.value - a.value);
-        setFilter(sortState);
+        setActiveValue(!activeValue);
         onCheckboxBtnClick(3);
     }
 
     const filterByDate = () => {
-        const date = new Date();
-        const currState = [...filter];
-        const newState = currState.filter(d =>
-            new Date(d.date) >=
-            new Date(String(date.getFullYear()) +
-                "-" +
-                String(date.getMonth() + 1) + "-01"
-            )
-        )
-        const sortState = newState.sort((a, b) => b.value - a.value);
-        setFilter(sortState);
+        setActiveDate(!activeDate);
         onCheckboxBtnClick(4);
     }
 
